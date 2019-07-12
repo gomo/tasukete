69d80e3123d335f287966000af864956a4b482d6
Masamoto Miyata <miyata@sincere-co.com>
2019-07-12 16:02:08 +0900

    execけしわすれ

--- init.bash
+++ init.bash
@@ -4,11 +4,11 @@
 tasukete() {
   if [ "$1" = "" ]
   then
-    echo -e "`ruby exec ~/tasukete/main.rb help`"
+    echo -e "`ruby ~/tasukete/main.rb help`"
     return 0
   fi
 
-  command=`ruby exec ~/tasukete/main.rb command $@`
+  command=`ruby ~/tasukete/main.rb command $@`
   eval "$command"
 }
 
