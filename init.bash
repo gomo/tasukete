ac0de4497bd92ea7c2387e31be67f650901b2c3e
Masamoto Miyata <miyata@sincere-co.com>
2019-07-12 16:01:02 +0900

    rubyに変更。

--- init.bash
+++ init.bash
@@ -4,11 +4,11 @@
 tasukete() {
   if [ "$1" = "" ]
   then
-    echo -e "`bundle exec ~/tasukete/main.rb help`"
+    echo -e "`ruby exec ~/tasukete/main.rb help`"
     return 0
   fi
 
-  command=`bundle exec ~/tasukete/main.rb command $@`
+  command=`ruby exec ~/tasukete/main.rb command $@`
   eval "$command"
 }
 
