13f75f3b6163804cba19b1282bcf029da19317ae
Masamoto Miyata <miyata@sincere-co.com>
2019-07-12 15:58:48 +0900

    パスを固定。

--- init.bash
+++ init.bash
@@ -4,11 +4,11 @@
 tasukete() {
   if [ "$1" = "" ]
   then
-    echo -e "`bundle exec main.rb help`"
+    echo -e "`bundle exec ~/tasukete/main.rb help`"
     return 0
   fi
 
-  command=`bundle exec main.rb command $@`
+  command=`bundle exec ~/tasukete/main.rb command $@`
   eval "$command"
 }
 
